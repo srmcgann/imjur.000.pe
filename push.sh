71deb4d0b2ffef4448f445e0c6faf00638814fe2
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-23 21:21:13 +0000

    sync

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull
 cd /home/whitehotrobot/imjur.000.pe
 cp /home/whitehotrobot/imjur.000.pe/dist/. /home/whitehotrobot/imjur.000.pe/dist_public/. -r
 php /home/whitehotrobot/imjur.000.pe/push_dir.php
