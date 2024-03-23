ad15c76bee56b92e673d2e9df4dbc377fcb4c61d
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-23 00:21:43 +0000

    sync

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
-    main section....
+    throw sum filez [drag/click]
+    <div class="dropTarget"></div>
   </div>
 </template>
 
@@ -23,9 +24,23 @@ export default {
   .main{
     background: #123;
     color: #ff0;
-    font-size: 32px;
+    font-size: 20px;
     overflow-y: auto;
     overflow-x: hidden;
     height: calc(100vh - 102px);
+    padding: 50px;
+    box-sizing: boorder-box;
+  }
+  .dropTarget:hover{
+    background-color: #032;
+  }
+  .dropTarget{
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 100% 100%;
+    background-image: url(loading.gif);
+    border: 1px solid #4f84;
+    background-color: #0320;
   }
 </style>
+
