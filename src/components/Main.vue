b6528c220c2810d28218711848268d9b00dff4a2
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-24 15:47:58 -0700

    Add files via upload

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -29,7 +29,21 @@ window.allowDrop = e => {
 
 window.drop = e => {
   e.preventDefault()
-  console.log('drop: ', e)
+  if (e.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    [...e.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        console.log(`… file[${i}].name = ${file.name}`);
+      }
+    });
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    [...e.dataTransfer.files].forEach((file, i) => {
+      console.log(`… file[${i}].name = ${file.name}`);
+    });
+  }
 }
 
 export default {
