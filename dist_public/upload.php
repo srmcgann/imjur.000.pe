7bb42e8b07dfcdb743e1e07865713e3922b56ae1
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-24 23:25:45 +0000

    sync

---
+++ dist_public/upload.php
@@ -0,0 +1,91 @@
+<?php
+
+  require_once('db.php');
+  require_once('functions.php');
+
+  $success = false;
+  $uploadDir = 'uploads';
+  $ct = 0;
+  $links = [];
+  $types = [];
+  $sizes = [];
+  if(sizeof($_FILES)){
+    forEach($_FILES as $key => $val){
+      $tmp_name = $_FILES["uploads_$ct"]['tmp_name'];
+      $slug = genSlug();
+      move_uploaded_file($tmp_name, "$uploadDir/$slug");
+      $type = mime_content_type("$uploadDir/$slug");
+      $continue = false;
+      $size = filesize("$uploadDir/$slug");
+      if($size < 1e8){ //~100MB
+        switch($type){
+          case 'image/jpg': $continue = true; $suffix = 'jpg'; break;
+          case 'image/jpeg': $continue = true; $suffix = 'jpeg';  break;
+          case 'image/png': $continue = true; $suffix = 'png';  break;
+          case 'image/gif': $continue = true; $suffix = 'gif';  break;
+          case 'image/webp': $continue = true; $suffix = 'webp';  break;
+          case 'video/webm': $continue = true; $suffix = 'webm';  break;
+          case 'video/mkv': $continue = true; $suffix = 'mkv';  break;
+          case 'video/mp4': $continue = true; $suffix = 'mp4';  break;
+          case 'audio/wav': $continue = true; $suffix = 'wav';  break;
+          case 'audio/mp3': $continue = true; $suffix = 'mp3';  break;
+          case 'audio/mpeg': $continue = true; $suffix = 'mp3';  break;
+        }
+        if($continue){
+          $hash = hash_file('md5', "$uploadDir/$slug");
+          $id = alphaToDec($slug);
+          $original_name = basename($_FILES["uploads_$ct"]["name"]);
+          $meta = mysqli_real_escape_string($link, json_encode([
+            "file size" => $size,
+            "sender IP" => $_SERVER['REMOTE_ADDR'],
+            "original name" => $original_name,
+          ]));
+          $meta = '';
+          $description = $_FILES["uploads_$ct"]["description"];
+          $origin = mysqli_real_escape_string($link, "user file: $original_name");
+          $userID = -1;
+          
+$sql = <<<SQL
+INSERT INTO imjurUploads (id, 
+                          slug,
+                          meta,
+                          hash,
+                          filetype,
+                          origin,
+                          userID,
+                          upvotes,
+                          downvotes,
+                          views,
+                          description
+                          )VALUES(
+                            $id,
+                            "$slug",
+                            "$meta",
+                            "$type",
+                            "$origin",
+                            $userID,
+                            0,
+                            0,
+                            0,
+                            "$description"
+                          )
+SQL;
+          
+          mysqli_query($link, $sql);
+          $success = true;
+          $links[] = "$uploadDir/$slug.$suffix";
+          $sizes[] = $size;
+          $types[] = $type;
+          rename("$uploadDir/$slug", "$uploadDir/$slug.$suffix");
+        }else{
+          unlink("$uploadDir/$slug");
+        }
+      }else{
+        unlink("$uploadDir/$slug");
+      }
+      $ct++;
+    }
+  }
+  
+  echo json_encode([$success, $links, $sizes, $types, $ct]);
+?>
