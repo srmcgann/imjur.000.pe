e78ddf57ef2f39bef134f2f3da9805be9327d827
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-26 17:43:58 +0000

    sync

---
+++ dist_public/functions.php
@@ -0,0 +1,38 @@
+<?php
+  function alphaToDec($val){
+    $pow=0;
+    $res=0;
+    while($val!=""){
+      $cur=$val[strlen($val)-1];
+      $val=substr($val,0,strlen($val)-1);
+      $mul=ord($cur)<58?$cur:ord($cur)-(ord($cur)>96?87:29);
+      $res+=$mul*pow(62,$pow);
+      $pow++;
+    }
+    return $res;
+  }
+
+  function decToAlpha($val){
+    $alphabet="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $ret="";
+    while($val){
+      $r=floor($val/62);
+      $frac=$val/62-$r;
+      $ind=(int)round($frac*62);
+      $ret=$alphabet[$ind].$ret;
+      $val=$r;
+    }
+    return $ret==""?"0":$ret;
+  }
+ 
+  function genSlug(){
+    global $link;
+    $rndmax = getrandmax();
+    do{
+      $newid = floor($rndmax/2+rand());
+      $sql = "SELECT id FROM imjurUploads WHERE id = $newid";
+      $res = mysqli_query($link, $sql);
+    }while(mysqli_num_rows($res));
+    return decToAlpha($newid);
+  }
+?>
