897935669f8b0df1df53b2cc304c0d2ceaaa2bda
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-26 18:00:43 -0700

    Add files via upload

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -102,6 +102,7 @@ export default {
         this.h = this.img.height
         this.Draw()
       }
+      let l
       this.img.src = this.linkType == 'audio' ? 'musicNote.png' : 'thumb.php?res=uploads/' + (l=this.link.href.split('/'))[l.length-1].split('?')[0]
     }
   }
