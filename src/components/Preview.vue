685d522529bbc29f61fbf3ad1837f49cfd1c4f8c
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-27 17:20:17 -0700

    Add files via upload

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -13,38 +13,7 @@
 export default {
   name: 'Preview',
   props: [ 'state', 'link' ],
-  data(){
-    return {
-      c: document.createElement('canvas'),
-      x: null,
-      linkType: '',
-      img: null,
-      w: 0,
-      h: 0,
-      t: 0,
-    }
-  },
   methods: {
-    Draw(){
-      this.x.globalAlpha = 1
-      this.x.fillStyle='#0008'
-      this.x.fillRect(0,0,this.c.width,this.c.height)
-      let fillStyle = 'contain'
-      let scl
-      switch(fillStyle){
-        case 'contain':
-          scl = this.c.width/this.c.height <= 1.777777778 ? this.c.width/this.w : this.c.height/this.h
-          break
-        case 'cover':
-          scl = this.c.width/this.c.height > 1.777777778 ? this.c.width/this.w : this.c.height/this.h
-          break
-      }
-      let w = this.w * scl
-      let h = this.h * scl
-      this.x.drawImage(this.img,this.c.width/2-w/2,this.c.height/2-h/2,w,h)
-      this.t += 1/60
-      if(this.t<2 || this.linkType == 'video') requestAnimationFrame(this.Draw)
-    },
     close(){
       this.state.closePreview()
     }
@@ -58,63 +27,33 @@ export default {
       case 'video': this.linkType = 'video'; break
     }
 
-
-    this.c.style.width = '200px'
-    this.c.style.height = '113px'
-    this.c.style.borderRadius = '20px'
-    if(0 && this.linkType == 'video'){
-      this.img = document.createElement('video')
-      this.img.loop = true
-      this.img.muted = true
-      this.img.oncanplay = () => {
-        this.w = this.img.videoWidth
-        this.h = this.img.videoHeight
-        this.c.width = this.w
-        this.c.height = this.h
-        this.img.play()
-        this.Draw()
-      }
-      this.img.src = this.link.href
+    if(this.linkType == 'image'){
+      this.img = document.createElement('div')
+      img.style.top = '50%'
+      img.style.left = '50%'
+      img.style.width = '100%'
+      img.style.height = '100%'
+      img.style.position = 'absolute'
+      img.style.objectFit = 'contain'
+      img.style.backgroundColor = '#000'
+      img.style.backgroundSize = 'contain'
+      img.style.backgroundRepeat = 'no-repeat'
+      img.style.backgroundPosition = 'center center'
+      img.style.backgroundImage = `url(${this.link.href})`
     }
-    if(this.linkType == 'image' || this.linkType == 'audio'){
-      this.img = new Image()
-      this.img.onload = () => {
-        this.w = this.img.width
-        this.h = this.img.height
-        this.c.width = this.w
-        this.c.height = this.h
-
-        let rsz = window.onresize = () => {
-          let el = document.body
-          let ar = el.clientWidth / el.clientHeight
-          let tgt_ar = this.w/this.h
-          let margin = 20
-          if(ar > tgt_ar){
-            this.c.style.height = (el.clientHeight - margin) + 'px'
-            this.c.style.width = (el.clientHeight * tgt_ar - margin) + 'px'
-          }else{
-            this.c.style.width = (el.clientWidth - margin) + 'px'
-            this.c.style.height = (el.clientWidth / tgt_ar - margin) + 'px'
-          }
-        }
-        rsz()
-
-        this.Draw()
+    if(this.linkType == 'audio'){
+      let audio = document.createElement('audio')
+      audio.controls = true
+      audio.style.position = 'absolute'
+      audio.style.left = '50%'
+      audio.style.top = '50%'
+      audio.loop = true
+      audio.style.transform = 'translate(-50%, -50%)'
+      audio.oncanplay = () => {
+        audio.play()
       }
-      if(this.linkType == 'audio'){
-        let audio = document.createElement('audio')
-        audio.controls = true
-        audio.style.position = 'absolute'
-        audio.style.left = '50%'
-        audio.style.top = '50%'
-        audio.loop = true
-        audio.style.transform = 'translate(-50%, -50%)'
-        audio.oncanplay = () => {
-          audio.play()
-        }
-        this.$refs.slideshow.appendChild(audio)
-        audio.src = this.link.href
-      }
+      this.$refs.slideshow.appendChild(audio)
+      audio.src = this.link.href
       let l
       this.img.src = this.linkType == 'audio' ? 'musicNote.png' : 'thumb.php?res=uploads/' + (l=this.link.href.split('/'))[l.length-1].split('?')[0]
     }
