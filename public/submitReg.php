56f887eed51d1b6951e45c5b56df04298b4f9f3a
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-29 13:12:54 -0700

    Add files via upload

--- public/submitReg.php
+++ public/submitReg.php
@@ -1,10 +1,13 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
   require_once('db.php');
   require_once('functions.php');
   $baseURL = $_SERVER['REQUEST_URI'];
 
   function escapeUserName($userName, $id){
-    return str_replace('\\', '', str_replace(';', '', str_replace("'", '', escapeshellarg(str_replace(' ', '', str_replace("\t", '', str_replace(';', '', str_replace("\n", '', str_replace('&', '', str_replace('|', '', str_replace('@', '', str_replace('#', '', str_replace('$', '', str_replace('%', '', str_replace('^', '', str_replace('(', '', str_replace(')', '', str_replace('?','', str_replace('!', '', str_replace('_', '', str_replace('`', '', str_replace("'", '', str_replace( '~', '', str_replace('"', '', $userName))))))))))))))))))))))).decToAlpha($id));
+    return str_replace('\\', '', str_replace(';', '', str_replace("'", '', (str_replace(' ', '', str_replace("\t", '', str_replace(';', '', str_replace("\n", '', str_replace('&', '', str_replace('|', '', str_replace('@', '', str_replace('#', '', str_replace('$', '', str_replace('%', '', str_replace('^', '', str_replace('(', '', str_replace(')', '', str_replace('?','', str_replace('!', '', str_replace('_', '', str_replace('`', '', str_replace("'", '', str_replace( '~', '', str_replace('"', '', $userName))))))))))))))))))))))).decToAlpha($id));
   }
 
 
@@ -17,7 +20,7 @@
   if($available && $password){
     $hash = password_hash($password, PASSWORD_DEFAULT);
     $sql = 'INSERT INTO imjurUsers (name, escaped_name, passhash, avatar) VALUES("'.$userName.'", "", "'.$hash.'","");';
-    mysqli_query($link, $sql);
+    //mysqli_query($link, $sql);
     $id = mysqli_insert_id($link);
     $escaped_name = escapeUserName($userName, $id);
     $sql = 'UPDATE imjurUsers SET escaped_name = "'.$escaped_name.'" WHERE id = ' . $id;
