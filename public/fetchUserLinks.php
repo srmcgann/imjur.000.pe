c24fbd603041061552c213058fdc3cd75e3a97df
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-30 16:54:44 -0700

    Add files via upload

--- public/fetchUserLinks.php
+++ public/fetchUserLinks.php
@@ -1,12 +1,25 @@
 <?php
   require('../db.php');
   $data = json_decode(file_get_contents('php://input'));
-  $userName = mysqli_real_escape_string($link, $data->{'userID'});
+  $userID = mysqli_real_escape_string($link, $data->{'userID'});
   $sql = "SELECT * FROM imjurUploads WHERE userID = $userID";
   $res = mysqli_query($link, $sql);
   $links = [];
   for($i=0; $i<mysqli_num_rows($res); ++$i){
-    $links[] = mysqli_fetch_assoc($res);
+    $row = mysqli_fetch_assoc($res);
+    $link = [
+      'id' => $row['id'],
+      'slug' => $row['slug'],
+      'filetype' => $row['filetype'],
+      'date' => $row['date'],
+      'userID' => $row['userID'],
+      'upvotes' => $row['upvotes'],
+      'downvotes' => $row['downvotes'],
+      'views' => $row['views'],
+      'description' => $row['description'],
+      'originalSlug' => $row['originalSlug'],
+    ]
+    $links[] = $link;
   }
   if(sizeof($links){
     echo json_encode([true, $links]);
