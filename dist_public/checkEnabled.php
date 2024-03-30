38fecbda93b754144fa7973808442b710e8c478a
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-30 23:31:47 +0000

    sync

--- dist_public/checkEnabled.php
+++ dist_public/checkEnabled.php
@@ -1,9 +1,9 @@
 <?php
-  require_once('../db.php');
+  require_once('db.php');
   $data = json_decode(file_get_contents('php://input'));
   $userName = mysqli_real_escape_string($link, $data->{'userName'});
   $passhash = mysqli_real_escape_string($link, $data->{'passhash'});
-  $sql = 'SELECT * FROM imjurUsers WHERE name LIKE "' . $userName . '" AND passhash LIKE "'.$passhash.'";';
+  $sql = 'SELECT * FROM imjurUsers WHERE name LIKE "' . $userName . '" AND passhash LIKE BINARY "'.$passhash.'";';
   $res = mysqli_query($link, $sql);
   if(mysqli_num_rows($res)){
     $row = mysqli_fetch_assoc($res);
