e29b5fdf7b4d84a81bdd455a5b492b691badb425
scott mcgann <s.r.mcgann@hotmail.com>
2024-03-31 00:20:23 +0000

    sync

---
+++ dist_public/fetchUserLinks.php
@@ -0,0 +1,32 @@
+<?php
+  require('../db.php');
+  $data = json_decode(file_get_contents('php://input'));
+  $userID = mysqli_real_escape_string($link, $data->{'userID'});
+  $sql = "SELECT * FROM imjurUploads WHERE userID = $userID";
+  $res = mysqli_query($link, $sql);
+  $links = [];
+  for($i=0; $i<mysqli_num_rows($res); ++$i){
+    $row = mysqli_fetch_assoc($res);
+    $meta = json_decode($row['meta']);
+    $size = $meta->{'file size'};
+    $link = [
+      'id' => $row['id'],
+      'size' => $size,
+      'href' => 'uploads/' . $row['slug'],
+      'type' => $row['filetype'],
+      'date' => $row['date'],
+      'userID' => $row['userID'],
+      'upvotes' => $row['upvotes'],
+      'downvotes' => $row['downvotes'],
+      'views' => $row['views'],
+      'description' => $row['description'],
+      'originalSlug' => $row['originalSlug'],
+    ];
+    $links[] = $link;
+  }
+  if(sizeof($links)){
+    echo json_encode([true, $links]);
+  }else{
+    echo json_encode([false]);
+  }
+?>
