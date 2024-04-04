35c38082260e96b54f5c8318a97913aec6b6a789
scott mcgann <s.r.mcgann@hotmail.com>
2024-04-04 23:27:45 +0000

    sync

--- dist_public/uploads/index.php
+++ dist_public/uploads/index.php
@@ -1,1 +1,32 @@
-nothing to see here!
+<?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  require('db.php');
+  $asset = mysqli_real_escape_string($link, $_GET['asset']);
+  if($asset){
+    $slug = explode('.', $asset)[0];
+    $suffix = explode('.', $asset)[1];
+    $sql = "SELECT * FROM imjurUploads WHERE slug LIKE BINARY \"$slug\"";
+    $res = mysqli_query($link, $sql);
+    if(mysqli_num_rows($res)){
+      $row = mysqli_fetch_assoc($res);
+      $id = $row['id'];
+      $filetype = $row['filetype'];
+      $sql = "UPDATE imjurUploads SET views = views + 1 WHERE id = $id";
+      mysqli_query($link, $sql);
+      $originalSlug = $row['originalSlug'];
+      $src = fopen("../resources/$originalSlug.$suffix", 'r');
+      stream_set_blocking($src, true);
+      header("Content-Type: $filetype");
+      do{
+        $data = fread($src, 1024*64);
+        if($data){
+          echo $data;
+          flush();
+          //ob_flush();
+        }
+      }while($data);
+    }
+  }
+?>
