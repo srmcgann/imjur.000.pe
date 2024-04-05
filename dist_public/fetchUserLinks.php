f3c7e1d53f46c5755645dd4d7e719fcc27de14a1
scott mcgann <s.r.mcgann@hotmail.com>
2024-04-05 20:15:32 +0000

    sync

--- dist_public/fetchUserLinks.php
+++ dist_public/fetchUserLinks.php
@@ -1,10 +1,25 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
   require_once('../db.php');
   require_once('functions.php');
   $data = json_decode(file_get_contents('php://input'));
   $userID = mysqli_real_escape_string($link, $data->{'userID'});
-  $sql = "SELECT * FROM imjurUploads WHERE userID = $userID";
+
+  $page = mysqli_real_escape_string($link, $data->{'page'});
+  $overrideMaxResults = mysqli_real_escape_string($link, $data->{'maxResultsPerPage'});
+  if($overrideMaxResults) $maxResultsPerPage = $overrideMaxResults;
+  $start = $maxResultsPerPage * $page;
+
+  $sql="SELECT id FROM imjurUploads WHERE userID = $userID";
   $res = mysqli_query($link, $sql);
+  $totalRecords = mysqli_num_rows($res);
+  $totalPages = floor(($totalRecords-1) / $maxResultsPerPage) + 1;
+
+  $sql = "SELECT * FROM imjurUploads WHERE userID = $userID ORDER BY date DESC LIMIT $start, $maxResultsPerPage";
+  $res = mysqli_query($link, $sql);
+  
   $uploadDir = 'uploads';
   $links = [];
   $meta = [];
@@ -47,7 +62,7 @@
     $meta[] = $m;
   }
   if(sizeof($links)){
-    echo json_encode([true, $links, $meta]);
+    echo json_encode([true, $links, $meta, $totalPages]);
   }else{
     echo json_encode([false]);
   }
