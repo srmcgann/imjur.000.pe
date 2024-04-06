65704e7891e72b7d8598391103d117f4d3a87c3c
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-06 15:01:57 -0700

    Add files via upload

--- public/uploads/index.php
+++ public/uploads/index.php
@@ -3,15 +3,16 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
   require('db.php');
+  require('../functions.php');
   $asset = mysqli_real_escape_string($link, $_GET['asset']);
   if($asset){
     $slug = explode('.', $asset)[0];
     $suffix = explode('.', $asset)[1];
-    $sql = "SELECT * FROM imjurUploads WHERE slug LIKE BINARY \"$slug\"";
+    $id = alphaToDec($slug);
+    $sql = "SELECT * FROM imjurUploads WHERE id = $id";
     $res = mysqli_query($link, $sql);
     if(mysqli_num_rows($res)){
       $row = mysqli_fetch_assoc($res);
-      $id = $row['id'];
       $filetype = $row['filetype'];
       $sql = "UPDATE imjurUploads SET views = views + 1 WHERE id = $id";
       mysqli_query($link, $sql);
@@ -20,7 +21,7 @@
       stream_set_blocking($src, true);
       header("Content-Type: $filetype");
       do{
-        $data = fread($src, 1024*64);
+        $data = fread($src, 1024*128);
         if($data){
           echo $data;
           flush();
