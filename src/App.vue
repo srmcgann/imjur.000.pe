92e602b64d687f8713f8a8969a8d79a613440945
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-06 16:47:05 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
   data(){
     return {
       state: {
-        footerMsg: '<b><span style="transform: scaleX(4.1);display: inline-block; margin-left: 86px; ">IMJUR</span></b><br><span style="transform: scaleX(.7);display: inline-block; margin-left: 86px;">A FREE DIGITAL ASSET<span><br>HOSTING SERVICE - ©'+(new Date()).getFullYear() + `<br>contact: <a href="mailto:whitehotrobot@gmail.com">whitehotrobot@gmail.com</a>`,
+        footerMsg: '<b><span style="transform: scaleX(4.1);display: inline-block; margin-left: 86px; ">IMJUR</span></b><br>A FREE DIGITAL ASSET<br><span style="transform: scaleX(.7);display: inline-block; margin-left: 86px;">HOSTING SERVICE - ©'+(new Date()).getFullYear() + `</span><br>contact: <a href="mailto:whitehotrobot@gmail.com">whitehotrobot@gmail.com</a>`,
         links: [],
         userLinks: [],
         uploadInProgress: false,
