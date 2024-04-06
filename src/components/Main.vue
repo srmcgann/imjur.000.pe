9335bea941cd9e150bd55882cc4fd9e3086beb8a
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-06 15:29:55 -0700

    Add files via upload

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -187,14 +187,16 @@ export default {
                 this.state.fetchUserLinks(this.state.loggedinUserID)
               }
             }
-            let data = JSON.parse(e.currentTarget.responseText)
-            if(data[0]){
-              data[1].map((v, j)=>{
-                //addLink(size, type, ct, href, selected, userID, slug, originalSlug, origin, serverTZO, views){
-                this.addLink(data[2][j], data[3][j], i, v, false, this.state.loggedinUserID, data[6][j], data[7][j], data[8][j], data[9], data[10][j], data[11][j])
-              })
-            }else{
-              this.state.modalContent = '<div style="min-width:90vw; min-height: 50vh; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);background: #8002; color: #f88; padding-top: 100px;">' + data[5] + '</div>'
+            if(e.currentTarget.responseText[0] != '<'){
+              let data = JSON.parse(e.currentTarget.responseText)
+              if(data[0]){
+                data[1].map((v, j)=>{
+                  //addLink(size, type, ct, href, selected, userID, slug, originalSlug, origin, serverTZO, views){
+                  this.addLink(data[2][j], data[3][j], i, v, false, this.state.loggedinUserID, data[6][j], data[7][j], data[8][j], data[9], data[10][j], data[11][j])
+                })
+              }else{
+                this.state.modalContent = '<div style="min-width:90vw; min-height: 50vh; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);background: #8002; color: #f88; padding-top: 100px;">' + data[5] + '</div>'
+              }
             }
           })
           request.send(data)
