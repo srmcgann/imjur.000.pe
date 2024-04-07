996fa1e51e3c01e9ec4bb5baa001a17ae9a3cbd7
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-06 17:25:11 -0700

    Add files via upload

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -190,6 +190,8 @@ export default {
                 this.state.jumpToPage(0)
                 this.state.links = []
                 this.state.fetchUserLinks(this.state.loggedinUserID)
+              }else{
+                this.state.modalContent = '<div style="min-width:90vw; min-height: 50vh; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);background: #0842; color: #8f8; padding-top: 100px;">' + `excellent choice, uploading here...<br><br>you have not logged in though, which means your links will be lost soon.<br><br>If you register, nothing is needed except a name of your choosing and a password you will remember, then these uploads will be auto-saved to your new profile...<br><br>just don't close the page or refresh before you login or register!<br><br><br>thanks for using imjur!` + '</div>'
               }
             }
             if(e.currentTarget.responseText[0] != '<'){
