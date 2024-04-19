22eba638b49ff4fa83b1a0c0ead7dfdfcf2ab4ef
Scott McGann <s.r.mcgann@hotmail.com>
2024-04-19 21:10:09 +0000

    sync

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/whitehotrobot/imjur.000.pe/
+cd ~/imjur.000.pe/
 git pull
 rm -rf dist_public
 mkdir dist_public
--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
-cd /home/whitehotrobot/imjur.000.pe
-cp /home/whitehotrobot/imjur.000.pe/dist/. /home/whitehotrobot/imjur.000.pe/dist_public/. -r
-php /home/whitehotrobot/imjur.000.pe/push_dir.php
+cd ~/imjur.000.pe
+cp ~/imjur.000.pe/dist/. ~/imjur.000.pe/dist_public/. -r
+php ~/imjur.000.pe/push_dir.php
 git add .
 git commit -m 'sync'
 git push
