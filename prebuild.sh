613b8f9b218336fe92f4a5c0fc346ca1b3b07ab4
scott mcgann <s.r.mcgann@hotmail.com>
2024-05-09 23:10:14 +0000

    sync

--- prebuild.sh
+++ prebuild.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-kill $(ps -ef | grep yarnbuild.sh | awk '{print $2}')
-screen -d -m ~/000assets/yarnbuild.sh
-screen -d -m ~/imjur.42web.io/build.sh
-screen -d -m ~/imjur.rf.gd/build.sh
-screen -d -m ~/assets.free.nf/build.sh
+
+#kill $(ps -ef | grep yarnbuild.sh | awk '{print $2}')
+#screen -d -m ~/000assets/yarnbuild.sh
+#screen -d -m ~/imjur.42web.io/build.sh
+#screen -d -m ~/imjur.rf.gd/build.sh
+#screen -d -m ~/assets.free.nf/build.sh
+
 cd ~/imjur.000.pe/
 git pull
 rm -rf dist_public
